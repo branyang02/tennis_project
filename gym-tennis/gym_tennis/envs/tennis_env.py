83433c16f4df97eea2e3b2d403de7f1cba1c9d26
branyang02 <branyang02@github.com>
2022-08-23 13:05:06 -0400

    second commit

--- gym-tennis/gym_tennis/envs/tennis_env.py
+++ gym-tennis/gym_tennis/envs/tennis_env.py
@@ -4,6 +4,21 @@ from gym import error, spaces
 from gym import utils
 from gym.utils import seeding
 
+from simulator import Speed, PositionStates
+from simulator import lower_bound_list, upper_bound_list, dof_states
+
+import torch
+import random
+
+
+#generate random action
+def GenerateAction():
+  for i in range(len(dof_states)):
+    random_action = random.uniform(lower_bound_list[i], upper_bound_list[i])
+    dof_states[i] = torch.tensor([random_action, 0], device="cuda:0")
+    return dof_states
+
+"""
 class FooEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
@@ -17,3 +32,4 @@ class FooEnv(gym.Env):
     ...
   def close(self):
     ...
+"""
